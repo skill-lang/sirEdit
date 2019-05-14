cfcb5aa4527609645a47f496602402e969a851a7
Marko Semet <Marko10_000@gmx.de>
2019-05-14 13:16:49 +0200

    Support types

--- src/gui/overview.cpp
+++ src/gui/overview.cpp
@@ -89,14 +89,24 @@ class Overview : public Gtk::VBox {
 		//
 		// Cache
 		//
+		std::unordered_set<const Type*> __cache_used_type;
+		std::unordered_set<const Type*> __cache_selected_type;
 		std::unordered_set<const Tool*> __cache_selected_tools;
 		std::unordered_set<const Tool*> __cache_used_tools;
+		const Tool* __current_tool;
 
 		void genCacheTools() {
 			this->__cache_selected_tools = {};
 			for(auto& i : this->tool_view.get_selection()->get_selected_rows()) {
 				const Tool* tmp = (*(this->tool_store->get_iter(i)))[toolModel.data_tool];
 				this->__cache_selected_tools.insert(tmp);
+			}
+		}
+		void genCacheTypes() {
+			this->__cache_selected_type = {};
+			for(auto& i : this->type_view.get_selection()->get_selected_rows()) {
+				const Type* tmp = (*(this->type_store->get_iter(i)))[typeModel.data_type];
+				this->__cache_selected_type.insert(tmp);
 			}
 		}
 
@@ -110,23 +120,61 @@ class Overview : public Gtk::VBox {
 			if(this->__cache_selected_tools.size() != 0) {
 				bool found = false;
 				for(auto& i : this->__cache_selected_tools) {
-					if(i == tool)
+					if(i == tool) {
 						found = true;
+						break;
+					}
 				}
 				if(!found)
 					return false;
 			}
-			// TODO: Check type marked
+
+			// Check type
+			if(this->__cache_selected_type.size() != 0) {
+				bool found = false;
+				for(auto& i : this->__cache_selected_type) {
+					if(tool->getTypeTransitiveState(*i) >= TYPE_STATE::READ) {
+						found = true;
+						break;
+					}
+				}
+				if(!found)
+					return false;
+			}
 			// TODO: Check field marked
+			return true;
+		}
+		bool isTypeActive(const Type* type) {
+			// Check selected types
+			if(this->__cache_selected_type.size() != 0) {
+				bool found = false;
+				for(auto& i : this->__cache_selected_type) {
+					if(i == type) {
+						found = true;
+						break;
+					}
+				}
+				if(!found)
+					return false;
+			}
+
+			// Check tools
+			if(this->__cache_selected_tools.size() != 0) {
+				bool found = false;
+				for(auto& i : this->__cache_selected_tools) {
+					if(i->getTypeTransitiveState(*type) >= TYPE_STATE::READ) {
+						found = true;
+						break;
+					}
+				}
+				if(!found)
+					return false;
+			}
+			// TODO: Check fields
 			return true;
 		}
 
 		void updateToolData() {
-			if(blockChange)
-				return;
-			blockChange = true;
-			std::cout << "test2" << std::endl;
-			this->genCacheTools();
 			this->tool_view.get_selection()->unselect_all();
 			this->tool_store->clear();
 			for(auto& i : this->view.getStaticView().getTools()) {
@@ -145,7 +193,54 @@ class Overview : public Gtk::VBox {
 				if(this->__cache_selected_tools.find(&i) != this->__cache_selected_tools.end())
 					this->tool_view.get_selection()->select(tmp);
 			}
-			blockChange = false;
+		}
+
+		template<bool FIRST>
+		void __genTypeItem(Gtk::TreeStore& tree, Gtk::TreeStore::Row& row, const Type* type) {
+			// Checks
+			if(this->hide_unused_type.property_active().get_value() && this->__cache_used_type.find(type) == this->__cache_used_type.end()) {
+				for(auto& i : type->getSubTypes())
+					this->__genTypeItem<FIRST>(tree, row, i);
+				return;
+			}
+			if(this->hide_inactive_type.property_active().get_value() && !this->isTypeActive(type)) {
+				for(auto& i : type->getSubTypes())
+					this->__genTypeItem<FIRST>(tree, row, i);
+				return;
+			}
+
+			// Insert
+			Gtk::TreeStore::Row own;
+			Gtk::TreeStore::iterator ownIter;
+			if constexpr(FIRST)
+				ownIter = tree.append();
+			else
+				ownIter = tree.append(row.children());
+			own = *ownIter;
+			own[typeModel.data_name] = type->getName();
+			own[typeModel.data_active] = this->isTypeActive(type);
+			own[typeModel.data_used] = this->__cache_used_type.find(type) != this->__cache_used_type.end();
+			own[typeModel.data_type] = const_cast<Type*>(type);
+			if(this->__cache_selected_type.find(type) != this->__cache_selected_type.end())
+				this->type_view.get_selection()->select(ownIter);
+			for(auto& i : type->getSubTypes())
+				this->__genTypeItem<false>(tree, own, i);
+		}
+		void updateTypeData() {
+			this->type_view.get_selection()->unselect_all();
+			this->type_store->clear();
+			{
+				Gtk::TreeStore::Row tmp;
+				for(auto& i : this->view.getStaticView().getBaseTypes())
+					this->__genTypeItem<false>(*(this->type_store.get()), tmp, i);
+				this->type_view.expand_all();
+			}
+		}
+
+		void __genFieldData() {
+
+		}
+		void updateFieldData() {
 		}
 
 		//
@@ -164,6 +259,7 @@ class Overview : public Gtk::VBox {
 			tree_view.append_column("name", tree_model.data_name);
 			tree_view.set_search_column(2);
 			tree_view.set_expander_column(*(tree_view.get_column(2)));
+			tree_view.expand_all();
 			tree_view.get_selection()->set_mode(Gtk::SelectionMode::SELECTION_MULTIPLE);
 		}
 
@@ -181,17 +277,37 @@ class Overview : public Gtk::VBox {
 				this->__cache_used_tools.erase(tmpInter);
 				tmp[toolModel.data_used] = false;
 			}
-			this->updateToolData();
+			this->update_all();
 		}
 		void toggle_type_used(const Glib::ustring& index) {
-			// TODO: Toogle used
+			auto tmp = *(this->type_store->get_iter(index));
+			auto tmpIter = this->__cache_used_type.find(static_cast<const Type*>(tmp[typeModel.data_type]));
+			if(tmpIter == this->__cache_used_type.end()) {
+				this->__cache_used_type.insert(static_cast<const Type*>(tmp[typeModel.data_type]));
+				tmp[typeModel.data_used] = true;
+			}
+			else {
+				this->__cache_used_type.erase(tmpIter);
+				tmp[typeModel.data_used] = false;
+			}
+			this->update_all();
 		}
 		void toggle_field_used(const Glib::ustring& index) {
 			// TODO: Toogle used
 		}
 
 		void update_all() {
+			if(blockChange)
+				return;
+			blockChange = true;
+
+			this->genCacheTools();
+			this->genCacheTypes();
 			this->updateToolData();
+			this->updateTypeData();
+			this->updateFieldData();
+
+			blockChange = false;
 		}
 
 	public:
@@ -208,28 +324,40 @@ class Overview : public Gtk::VBox {
 				Gtk::VBox* tmp = Gtk::manage(new Gtk::VBox());
 				tmp->pack_start(this->hide_inactive_tool, false, true);
 				this->hide_inactive_tool.signal_toggled().connect([this]() -> void {
-					this->updateToolData();
+					this->update_all();
 				});
 				tmp->pack_start(this->hide_unused_tool, false, true);
 				this->hide_unused_tool.signal_toggled().connect([this]() -> void {
-					this->updateToolData();
+					this->update_all();
 				});
-				tmp->pack_start(this->tool_view, true, true);
+				Gtk::ScrolledWindow* tmp2 = Gtk::manage(new Gtk::ScrolledWindow());
+				tmp2->add(this->tool_view);
+				tmp->pack_start(*tmp2, true, true);
 				this->tool_paned.pack1(*tmp);
 			}
 			this->type_paned.pack2(this->field_paned);
 			{
 				Gtk::VBox* tmp = Gtk::manage(new Gtk::VBox());
 				tmp->pack_start(this->hide_inactive_type, false, true);
+				this->hide_inactive_type.signal_toggled().connect([this]() -> void {
+					this->update_all();
+				});
 				tmp->pack_start(this->hide_unused_type, false, true);
-				tmp->pack_start(this->type_view, true, true);
+				this->hide_unused_type.signal_toggled().connect([this]() -> void {
+					this->update_all();
+				});
+				Gtk::ScrolledWindow* tmp2 = Gtk::manage(new Gtk::ScrolledWindow());
+				tmp2->add(this->type_view);
+				tmp->pack_start(*tmp2, true, true);
 				this->type_paned.pack1(*tmp);
 			}
 			{
 				Gtk::VBox* tmp = Gtk::manage(new Gtk::VBox());
 				tmp->pack_start(this->hide_inactive_field, false, true);
 				tmp->pack_start(this->hide_unused_field, false, true);
-				tmp->pack_start(this->field_view, true, true);
+				Gtk::ScrolledWindow* tmp2 = Gtk::manage(new Gtk::ScrolledWindow());
+				tmp2->add(this->field_view);
+				tmp->pack_start(*tmp2, true, true);
 				this->field_paned.pack1(*tmp);
 			}
 			{
@@ -251,6 +379,9 @@ class Overview : public Gtk::VBox {
 			this->type_view.set_model(this->type_store);
 			genTreeView(this->type_view, typeModel, [this](Glib::ustring index) -> void {
 				this->toggle_type_used(index);
+			});
+			this->type_view.get_selection()->signal_changed().connect([this]() -> void {
+				this->update_all();
 			});
 
 			this->field_store = Gtk::TreeStore::create(fieldModel);
