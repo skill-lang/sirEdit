6d73225f9aa6b8e76eff49c3b16882eef9ee6515
Marko Semet <Marko10_000@gmx.de>
2019-06-22 17:44:30 +0200

    Test for recoursion

---
+++ src/unittests/rules/rule7.cpp
@@ -0,0 +1,62 @@
+#include "model.hpp"
+#include <gtest/gtest.h>
+#include <sirEdit/data/tools.hpp>
+
+using namespace sirEdit::data;
+
+TEST(rule_7, DirectSubclass) {
+	// Values
+	TypeTestModel1 model;
+	Tool t;
+
+	// Set state test
+	t.setTypeState(model.g, TYPE_STATE::READ);
+	EXPECT_EQ(t.getTypeTransitiveState(model.g), TYPE_STATE::READ);
+	EXPECT_EQ(t.getTypeTransitiveState(model.a), TYPE_STATE::READ);
+	EXPECT_EQ(t.getTypeTransitiveState(model.i), TYPE_STATE::UNUSED);
+	EXPECT_EQ(t.getTypeTransitiveState(model.b), TYPE_STATE::UNUSED);
+
+	// Reset state
+	t.setTypeState(model.g, TYPE_STATE::UNUSED);
+	EXPECT_EQ(t.getTypeTransitiveState(model.g), TYPE_STATE::UNUSED);
+	EXPECT_EQ(t.getTypeTransitiveState(model.a), TYPE_STATE::UNUSED);
+	EXPECT_EQ(t.getTypeTransitiveState(model.i), TYPE_STATE::UNUSED);
+	EXPECT_EQ(t.getTypeTransitiveState(model.b), TYPE_STATE::UNUSED);
+}
+
+TEST(rule_7, DirectInterface) {
+	// Values
+	TypeTestModel1 model;
+	Tool t;
+
+	// Set state test for b
+	t.setTypeState(model.b, TYPE_STATE::READ);
+	EXPECT_EQ(t.getTypeTransitiveState(model.b), TYPE_STATE::READ);
+	EXPECT_EQ(t.getTypeTransitiveState(model.a), TYPE_STATE::READ);
+	EXPECT_EQ(t.getTypeTransitiveState(model.c), TYPE_STATE::UNUSED);
+	EXPECT_EQ(t.getTypeTransitiveState(model.i), TYPE_STATE::UNUSED);
+
+	// Set state test for b and c
+	// Additional cache poisen test
+	t.setTypeState(model.c, TYPE_STATE::READ);
+	EXPECT_EQ(t.getTypeTransitiveState(model.b), TYPE_STATE::READ);
+	EXPECT_EQ(t.getTypeTransitiveState(model.a), TYPE_STATE::READ);
+	EXPECT_EQ(t.getTypeTransitiveState(model.c), TYPE_STATE::READ);
+	EXPECT_EQ(t.getTypeTransitiveState(model.i), TYPE_STATE::UNUSED);
+
+	// Set state test for c
+	// Addition cache poisen test
+	t.setTypeState(model.b, TYPE_STATE::UNUSED);
+	EXPECT_EQ(t.getTypeTransitiveState(model.b), TYPE_STATE::UNUSED);
+	EXPECT_EQ(t.getTypeTransitiveState(model.a), TYPE_STATE::READ);
+	EXPECT_EQ(t.getTypeTransitiveState(model.c), TYPE_STATE::READ);
+	EXPECT_EQ(t.getTypeTransitiveState(model.i), TYPE_STATE::UNUSED);
+
+	// Reset state
+	// Addition cache poisen test
+	t.setTypeState(model.c, TYPE_STATE::UNUSED);
+	EXPECT_EQ(t.getTypeTransitiveState(model.b), TYPE_STATE::UNUSED);
+	EXPECT_EQ(t.getTypeTransitiveState(model.a), TYPE_STATE::UNUSED);
+	EXPECT_EQ(t.getTypeTransitiveState(model.c), TYPE_STATE::UNUSED);
+	EXPECT_EQ(t.getTypeTransitiveState(model.i), TYPE_STATE::UNUSED);
+}
