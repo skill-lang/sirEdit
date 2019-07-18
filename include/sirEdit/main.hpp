8be363052f5fc1278f64749f715f3fe2db768b22
Marko Semet <marko10_000@gmx.de>
2019-07-18 09:57:39 +0200

    main functions

--- include/sirEdit/main.hpp
+++ include/sirEdit/main.hpp
@@ -9,12 +9,38 @@
 namespace sirEdit {
 	extern Gtk::Application* mainApplication;
 
+	/**
+	 * Save the file
+	 * @param blocking should block till it's done
+	 */
 	extern void doSave(bool blocking=false);
+	/**
+	 * Returns path where it's saved (remotly)
+	 * @param saved path
+	 */
 	extern std::string getSavePath();
+	/**
+	 * Returns the current sir file path
+	 * @return sir saved path
+	 */
 	extern std::string getSirPath();
+	/**
+	 * Gnerates a tool spec
+	 * @param tool the toolspec that should be used. When empty global sepc
+	 * @return The serialized skill spec
+	 */
 	extern std::string getSpec(const sirEdit::data::Tool* tool);
 
+	/**
+	 * Load a file and then close the window
+	 * @param window the window to close
+	 * @param chooser the source of the data
+	 */
 	extern void loadFile(Gtk::Window* window, Gtk::FileChooserNative* chooser);
+	/**
+	 * Run an lambda in the gui thread
+	 * @param func the function that should run
+	 */
 	extern void runInGui(std::function<void()> func);
 	/**
 	 * Run a codegenerator in a directory. It will export the codegenerator automatically
